603d1092aaaf03e9d1256ee63764493834c7c5a8
wtc <onzwanejhb024@student.wethinkcode.co.za>
2025-06-27 15:39:27 +0200

    HTML and CSS translation to REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>good day</h1>
+<header>
+    <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<Home/>}/>
+            <Route path={"/work"} element={<Work/>}/>
+            <Route path={"/about"} element={<AboutMe/>}/>
+
+
+        </Routes>
+    </BrowserRouter>
+
+</header>
+
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Hey home</h1>
+            <ul className="nav justify-content-end">
+                <li className="nav-item">
+                    <a id="work" className="nav-link active" href="/work" target="_blank">Work</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/about" target="_blank">About me</a>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Home;
